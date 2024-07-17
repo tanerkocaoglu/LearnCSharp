6c9039bbfec8e36aaba5ee17570972ebe7e065ae
tanerkocaoglu <tanerkocaoglu14@gmail.com>
2024-07-17 18:54:53 +0300

    keywordler eklendi

---
+++ Keywords/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Keyword : Programlama dilinin en atomik parçalarıdır.
+        //Derleyici için ön tanımlı olan nerede ne amaca hizmet edeceği billi ve kod içerisinde hangi noktalarda çağrılabileceği kurallarla sınırlandırılmış ön tanımlardır.
+
+    }
+}
